90e2ce3cbbc4eec26789c543911316589a7300ec
方友镜 <fang799336159@163.com>
2017-06-04 22:13:07 +0800

    首页功能样式功能完成

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,42 @@
 /**
  * Created by Jing on 2017/6/3.
  */
+    //自己封装的根据不同的浏览器实现滑动效果
+    function scroll() {
+        //判断当前的浏览器是否是IE9+ 或者其他浏览器
+        if (window.pageYOffset != null) {
+            return {
+                left: window.pageXOffset,
+                top: window.pageYOffset
+            };
+        }
+        //检测是否是怪异模式浏览器，就是没有声明<!DOCTYPE html>
+        else if(document.compatMode == "CSS1Compat"){
+            //正常模式
+            return {
+                left: document.documentElement.scrollLeft,
+                top: document.documentElement.scrollTop
+            };
+        }
+        //怪异浏览器
+        return {
+            left:document.body.scrollLeft,
+            top:document.body.scrollTop
+        };
+    }
+    	window.onscroll = function () {
+    	//backgroundAttachment 属性设置背景图像是否固定或者随着页面的其余部分滚动。
+        var dis = scroll().top;
+        if(dis > 0&& dis<1599){
+            document.body.style.backgroundImage ="url(img/body2.jpg)";         
+            document.body.style.backgroundAttachment = "fixed";
+        }
+        else if(dis > 1600&& dis<6000){
+            document.body.style.backgroundImage = "url(img/body1.jpg)";
+            document.body.style.backgroundAttachment = "fixed";
+        }else{
+        	 document.body.style.background="white";
+           
+        }
+    }
+    	
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
-/**
- * Created by Jing on 2017/6/3.
- */
+var gulp=require("gulp");
+var less=require("less");
+var $=require('gulp-load-plugins')();
+// var cleancss=require("gulp-clean-css");//压缩代码
+// var concat=require("gulp-concat");
+//less文件编译成css文件并压缩
+gulp.task('less',function () {
+    return gulp.src('app/styles/*.less')
+        .pipe($.less())
+//         .pipe(cleancss())//压缩
+        .pipe(gulp.dest('dist/css'));
+});
+/*当改变代码的时候改变*/
+gulp.task("dist",function () { 
+    gulp.watch("app/styles/*.less",['less']);//编译和压缩CSS文件
+});
